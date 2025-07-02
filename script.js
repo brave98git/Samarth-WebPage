fd59c07cbc96442ed9ec3ed4e7e14b68c4a51294
Samarth <samarthpawarsamarthpawar@gmail.com>
2025-07-02 21:13:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const text = "Hello Everyone! I'm Samarth";
+const speed = 100;
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typewriter").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+window.onload = typeWriter;
